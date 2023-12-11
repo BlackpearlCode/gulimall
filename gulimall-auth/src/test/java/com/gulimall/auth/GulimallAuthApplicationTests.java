847cca6157e10477a31d6dc7faa241b1884424e5
william <2644985344@qq.com>
2023-12-11 15:51:04 +0800

    新增认证服务模块

---
+++ gulimall-auth/src/test/java/com/gulimall/auth/GulimallAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gulimall.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
