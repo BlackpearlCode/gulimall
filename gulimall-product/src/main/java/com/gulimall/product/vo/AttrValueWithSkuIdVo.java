14ec67f4172d2bd14af80f01e214d757abd94bbb
william <2644985344@qq.com>
2023-12-11 10:43:52 +0800

    sku组合切换

---
+++ gulimall-product/src/main/java/com/gulimall/product/vo/AttrValueWithSkuIdVo.java
@@ -0,0 +1,9 @@
+package com.gulimall.product.vo;
+
+import lombok.Data;
+
+@Data
+public class AttrValueWithSkuIdVo {
+    private String attrValue;
+    private String skuIds;
+}
--- gulimall-product/src/main/java/com/gulimall/product/vo/SkuItemSaleAttrVo.java
+++ gulimall-product/src/main/java/com/gulimall/product/vo/SkuItemSaleAttrVo.java
@@ -8,5 +8,5 @@ import java.util.List;
 public class SkuItemSaleAttrVo {
     private Long attrId;
     private String attrName;
-    private String attrValues;
+    private List<AttrValueWithSkuIdVo> attrValues;
 }
