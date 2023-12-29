ad0e0db9303b566e28819e3e26a049a59d0635fb
william <2644985344@qq.com>
2023-12-29 22:57:17 +0800

    rabbitmq发送，接收消息

--- gulimall-order/src/main/java/com/example/order/service/serviceImpl/OrderServiceImpl.java
+++ gulimall-order/src/main/java/com/example/order/service/serviceImpl/OrderServiceImpl.java
@@ -1,5 +1,10 @@
 package com.example.order.service.serviceImpl;
 
+import com.example.order.entity.OrderReturnReason;
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.order.mapper.OrderMapper;
 import com.example.order.entity.Order;
 import com.example.order.service.OrderService;
+
+import java.io.IOException;
+
+@RabbitListener(queues = {"hello-java-queue"})
 @Service
 public class OrderServiceImpl implements OrderService{
 
@@ -41,6 +50,22 @@ public class OrderServiceImpl implements OrderService{
     @Override
     public int updateByPrimaryKey(Order record) {
         return orderMapper.updateByPrimaryKey(record);
+    }
+
+    @RabbitHandler
+    public void receieveMessage(Message message, OrderReturnReason content, Channel channel) throws IOException {
+        System.out.println("收到消息：" + content);
+        long deliveryTag = message.getMessageProperties().getDeliveryTag();
+        System.out.println("deliveryTag--->"+deliveryTag);
+        //手动签收
+        channel.basicAck(deliveryTag,false);
+        /**
+         * 拒绝消息
+         * deliveryTag：消息的唯一标识
+         * multiple：是否批量拒绝
+         * requeue：是否重新入队；true：重新入队，false：不重新入队
+         */
+        // channel.basicNack(deliveryTag,false,true);
     }
 
 }
--- gulimall-order/src/test/java/com/example/order/GulimallOrderApplicationTests.java
+++ gulimall-order/src/test/java/com/example/order/GulimallOrderApplicationTests.java
@@ -1,72 +1,12 @@
 package com.example.order;
 
-import com.example.order.entity.OrderReturnReason;
-import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Date;
 
-@Slf4j
 @SpringBootTest
 class GulimallOrderApplicationTests {
 
-    @Autowired
-    private AmqpAdmin amqpAdmin;
 
-    @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    /**
-     * 1.创建Exchange、Queue、Binding
-     *  使用AmqpAdmin进行创建
-     * 2.如何收发消息
-     */
-    @Test
-    void contextLoads() {
-        //创建交换机
-        DirectExchange directExchange = new DirectExchange("hello-java-exchange",true,true,null);
-        amqpAdmin.declareExchange(directExchange);
-        log.info("Exchange创建成功");
-    }
-
-    @Test
-    void createQueue(){
-        /**
-         * String name, boolean durable, boolean exclusive, boolean autoDelete,
-         * 			            @Nullable Map<String, Object> arguments
-         */
-        Queue queue = new Queue("hello-java-queue",true,false,true,null);
-        amqpAdmin.declareQueue(queue);
-        log.info("Queue创建成功");
-    }
-
-    @Test
-    void createBinding(){
-        /**
-         * @Nullable Queue lazyQueue, @Nullable String destination, DestinationType destinationType,
-         * 			String exchange, @Nullable String routingKey, @Nullable Map<String, Object> arguments
-         */
-        //将exchange指定的交换机和destination目的地进行绑定，使用routingkey作为知道的的路由键
-        Binding binding = new Binding("hello-java-queue", Binding.DestinationType.QUEUE,"hello-java-exchange","hello.java",null);
-        amqpAdmin.declareBinding(binding);
-        log.info("Binding创建成功");
-    }
-
-    @Test
-    void senMessageTest(){
-        OrderReturnReason orderReturnReason = new OrderReturnReason();
-        orderReturnReason.setId(1L);
-        orderReturnReason.setCreateTime(new Date());
-        orderReturnReason.setName("哈哈哈");
-        //1.发送消息
-        rabbitTemplate.convertAndSend("hello-java-exchange","hello.java",orderReturnReason);
-        log.info("消息发送成功{}",orderReturnReason);
-    }
 
 
 
